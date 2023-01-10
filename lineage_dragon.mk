1484137c183c4ba74f9d3a79c4b133326c3470a3
Tobias Tefke <tobias@tefke.name>
2023-01-10 09:39:59 +0100

    Remove product.mk
    Change-Id: Ia91aa009c9831843389f4f945dfd8b7abf4859a7

--- lineage_dragon.mk
+++ lineage_dragon.mk
@@ -22,8 +22,11 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, device/google/dragon/product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from device
+$(call inherit-product, device/google/dragon/device.mk)
 
 # Inherit some common MK stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
